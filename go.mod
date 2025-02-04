fd8aafcf5325e440841b9760677945f9a3ce8df0
Anoop John <anoop.john@logicline.de>
2025-02-04 15:16:34 +0530

    sample ai program with langchain

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/anoopjohn02/ai-golang-sample
+
+go 1.22.0
+
+toolchain go1.22.11
+
+require (
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/pkoukk/tiktoken-go v0.1.6 // indirect
+	github.com/tmc/langchaingo v0.1.12 // indirect
+)
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/openai"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	
+	ctx := context.Background()
+	llm, err := openai.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	prompt := "What would be a good company name for a company that makes colorful socks?"
+	completion, err := llms.GenerateFromSinglePrompt(ctx, llm, prompt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(completion)
+}
