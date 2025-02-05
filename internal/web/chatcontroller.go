bfcb394da7d0a8397498b010ce6878fb2ddab045
Anoop John <anoop.john@logicline.de>
2025-02-05 22:32:01 +0530

    stream llm messages

--- internal/web/chatcontroller.go
+++ internal/web/chatcontroller.go
@@ -1,10 +1,13 @@
 package web
 
 import (
+	"context"
 	"fmt"
-	"time"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/openai"
 )
 
 type ChatController struct {
@@ -20,13 +23,26 @@ func (s *ChatController) StreamChat(c *gin.Context) {
 	c.Writer.Header().Set("Connection", "keep-alive")
 	c.Writer.Header().Set("Transfer-Encoding", "chunked")
 
-	c.Writer.Flush()
-	for i := 0; i < 10; i++ {
-		_, err := fmt.Fprintf(c.Writer, "data: Message %d ", i)
-		if err != nil {
-			return
-		}
-		c.Writer.Flush()
-		time.Sleep(1 * time.Second)
+	ctx := context.Background()
+	llm, err := openai.New()
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	prompt := "Tell me a story about a wise old owl."
+
+	content := []llms.MessageContent{
+		llms.TextParts(llms.ChatMessageTypeSystem, "You are a story teller."),
+		llms.TextParts(llms.ChatMessageTypeHuman, prompt),
+	}
+	completion, err := llm.GenerateContent(ctx, content, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+		fmt.Fprintf(c.Writer, string(chunk))
+		c.Writer.Flush()
+		return nil
+	}))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = completion
+
 }
